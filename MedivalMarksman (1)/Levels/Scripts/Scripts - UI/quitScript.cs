dcd2b1b995a16807c4d51075caea9baeb6443299
hebiqwse <hebiqwse@users.noreply.github.com>
2019-11-24 11:49:20 +0000

    Changes
    Added stone arrow from google drive
    Changed folders in "Levels"

---
+++ MedivalMarksman (1)/Levels/Scripts/Scripts - UI/quitScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitScript : MonoBehaviour
+{
+   
+    public void doquit()
+    {
+        Debug.Log("has quit game");
+        Application.Quit();  
+    }
+
+}
