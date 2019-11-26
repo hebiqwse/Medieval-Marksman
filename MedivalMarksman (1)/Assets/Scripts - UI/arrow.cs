3b03ebc5017ea9b465aa916a9a98ac024713b949
sdmay1 <45387452+sdmay1@users.noreply.github.com>
2019-11-26 14:21:22 +0000

    Windzone
    Added the working windzone into the turorial level

---
+++ MedivalMarksman (1)/Assets/Scripts - UI/arrow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+    public bool inWindZone = false;
+    public GameObject windZone;
+    Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+  private  void FixedUpdate()
+    {
+        if (inWindZone)
+        {
+            rb.AddForce(windZone.GetComponent<windArea>().direction * windZone.GetComponent<windArea>().strength);
+        }
+    }
+
+    void OnTriggerEnter (Collider collider)
+    {
+        if (collider.gameObject.tag == "windArea")
+        {
+            windZone = collider.gameObject;
+            inWindZone = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "windArea")
+            inWindZone = false;
+    }
+
+
+    
+}
---
+++ MedivalMarksman (1)/Assets/Scripts - UI/gravity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravity : MonoBehaviour
+{
+    public bool useGravity = true;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        rb.useGravity = false;
+        if(useGravity)
+            rb.AddForce(Physics.gravity * (rb.mass *rb.mass));
+    }
+}
