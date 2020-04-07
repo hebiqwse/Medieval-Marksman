47d9e67f41815b392cb32c2077a0963a7977b8df
FMSmith1 <44603852+FMSmith1@users.noreply.github.com>
2020-04-07 17:49:13 +0100

    SoundManager

---
+++ MedivalMarksman (1)/Assets/Scripts - UI/SoundManagerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManagerScript : MonoBehaviour
+{
+    //Initialise Sound variables
+    public static AudioClip cheerSound, clickSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Loads sound values to the variables
+        cheerSound = Sounds.Load<AudioClip>("cheer");
+        clickSound = Sounds.Load<AudioClip>("click");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "cheer":
+                audioSrc.PlayOneShot(cheerSound);
+                break;
+            case "click":
+                audioSrc.PlayOneShot(clickSound);
+                break;
+        }
+    }
+
+}
