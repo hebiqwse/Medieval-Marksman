91a054989771c0e2a0ac6feceeeb2af6c66a13cc
FMSmith1 <44603852+FMSmith1@users.noreply.github.com>
2020-04-08 07:12:35 +0100

    Removed Sound Manager Temporarily
    Created Code, but seem to be having complier issues and saved the code externally.

--- MedivalMarksman (1)/Assets/Scripts - UI/MainMenu.cs
+++ MedivalMarksman (1)/Assets/Scripts - UI/MainMenu.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        
+        soundManagerScript.PlaySound("click");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  
     }
 
     public void quitGame()
     {
+        soundManagerScript.PlaySound("click");
         Application.Quit();
         Debug.Log("you have quit"); 
     }
