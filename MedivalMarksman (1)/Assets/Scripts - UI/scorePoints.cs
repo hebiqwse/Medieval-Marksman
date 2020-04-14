417e70f2dee051d448b4ffaf0803404c4e7db56b
markperkins123 <46870026+markperkins123@users.noreply.github.com>
2020-04-14 17:05:52 +0100

    Commented out the sound manage code.
    The score bug has been fixed.

--- MedivalMarksman (1)/Assets/Scripts - UI/scorePoints.cs
+++ MedivalMarksman (1)/Assets/Scripts - UI/scorePoints.cs
@@ -53,7 +53,7 @@ public class scorePoints : MonoBehaviour
         if (score.currentScore >= 100)
         {
             //If the score is higher thant 100, load the level clear screen
-            SoundManagerScript.PlaySound("cheer");
+            //SoundManagerScript.PlaySound("cheer");
             SceneManager.LoadScene("LevelClear");
             resetScore();
         }
